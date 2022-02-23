d41b0b5458201bed0d42f9e120bf3be53ab706cb
Scleepy <danielyohanes03@gmail.com>
2022-02-23 13:28:48 +0700

    first commit

---
+++ Day 6 - Escaping The Maze/hurdle1.py
@@ -0,0 +1,17 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def hop():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+     hop()
