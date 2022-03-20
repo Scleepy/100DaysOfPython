3a86aaec85017155feeae89d5d31728e793d8bf2
Scleepy <danielyohanes03@gmail.com>
2022-03-20 21:27:15 +0700

    Day 21

---
+++ Day 21 - Snake Game Part 2/main.py
@@ -0,0 +1,47 @@
+import turtle
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = turtle.Screen()
+
+screen.setup(width = 600, height = 800)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
+game_state = True
+
+while game_state:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_state = False
+
+    for seg in snake.segments:
+        if seg == snake.head:
+            pass
+        elif snake.head.distance(seg) < 10:
+            scoreboard.game_over()
+            game_state = False
+
+screen.exitonclick()
