cc9f31827660f8c0275cfe613c8a3a56eaf494f3
Scleepy <danielyohanes03@gmail.com>
2022-04-01 13:29:05 +0700

    Day 24

---
+++ Day 24 - Directories/FILE IO/main.py
@@ -0,0 +1,12 @@
+#READING FILES
+# with open ("myfile.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+#WRITING FILES
+# with open ("myfile.txt", "w") as file:
+#     file.write("Poggers")
+
+#APPENDING FILES
+with open ("myfile.txt", "a") as file:
+    file.write("\nPoggers")
